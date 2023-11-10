3fca91c8b75a70a47cdd5678558266c47b06612e
DTRHnet <145516411+DTRHnet@users.noreply.github.com>
2023-11-10 08:49:26 -0500

    Update textlib.sh
    few more changes and more greentext

--- textlib.sh
+++ textlib.sh
@@ -8,16 +8,17 @@
 # Library of text related functions including but  not limited to stylizing text, 
 # counters, and I/O operations.
 #
+# Latest Update: Nov 10, 2023
 # #################################################################################
 
-
+# LOGGING (TODO)
 shopt -s expand_aliases
 _LOG_='.textlib.log' && touch "${_LOG_}"
 
 
 # #################################################################################
 # COLOUR ALIASES
-
+#
 alias red="echo 196";     alias lred="echo 1";      alias dred="echo 88"
 alias green="echo 2";     alias lgreen="echo 155";  alias dgreen="echo 28"
 alias blue="echo 4";      alias lblue="echo 12";    alias dblue="echo 27"
@@ -26,46 +27,75 @@ alias orange="echo 208";  alias lorange="echo 214"; alias dorange="echo 160"
 alias white="echo 255";   alias lgrey="echo 252";   alias dgrey="echo 238"
 alias black="echo 0" 
 
-# COLOUR GLOBALS
-fg=255    # Foreground
-bg=000    # Background
-b=0       # Bold
+# COLOUR GLOBALS ##################################################################
+#
+fg=255    # Foreground # White (These can be set to any of the aliases above)
+bg=000    # Background # Black "
+b=0       # Standard
 
 
 # #################################################################################
 # FUNCTIONS
+# #################################################################################
 
-# C : Convert colour alias to appropriate 256 byte pointing to colour
-#     c returns in the form of the byte echoing. 
-#     It's most easily used via command substitution ie:
+function c() { 
+# c() #############################################################################
+# 
+# Usage: setTextColour $(c red) $(c dgrey) 1 "c returns the colour byte value"    
+#        c returns the value via 'echo'.. 
+#        It was designed to work with command substitution in mind. This is why the
+#        function itself is only onee character long (to save on unnecessary wording)
 #
-#     $(c alias)    # Substitute this in place of a byte colour code 
-function c() { alias cc="$1" && fg=$(cc); echo -n $fg; }
+# Example: echo -e '[all aliases]' > /tmp/alias.txt && \
+#          sed -i -e 's/;/\n/g' /tmp/alias.txt && sed -i -e 's/=/ /g' /tmp/alias.txt && \
+#          awk -F' ' '{print $2}'/tmp/alias.txt && IFS\"\n"
+#          while read -r line; do
+#             secho $line 0 "Colour Code: $line"
+#          done < /tmp/alias.txt
+#                            
+
+alias cFg="$1"
+alias cBg="$2"
+
+fg=$(cFg)
+bg=$(cFg)
+
+[ ! $fg ] && fg=0      # TODO: optimize this, like checking if $2 is
+[ ! $bg ] && bg=255    # absent, but $bg has a value. If thats the case it should
+                       # not reset the bg colour..
+
+echo -e "$fg $bg"   # To catch in subcommand
+
+}
 
 
 function secho() { 
-
+# secho() #########################################################################
 # "Stylized Echo"
 #
-# secho COLOUR[ALIAS/BYTE] BOLD[0-1] "STRING"  
+# secho COLOUR[alias;byte] BOLD[0-1] "str.."  
 #
-# Usage:    1.  secho red 0 "STRING"  
-#           2.  secho 122 1 "STRING"
+# Usage:    1.  secho red 0 "str.."  
+#           2.  secho 122 1 "str.."
 #                                                                                 
 # Summary:  Style a string of text. Text colours return to default after after 
 #           execution. This function does not set background colours.
+#           It resets term colours after displaying the text string
 #           
 # #################################################################################
 
   echo -ne $(tput setaf $(c "$1")) 
   if [ $2 = 1 ]; then echo -ne $(tput bold); fi 
+
+  # Reset the terminal. This is intentional.
   echo $3 && echo -ne $(tput sgr0)
 
 }
 
 
 function setTerm() {
-
+# setTerm() #########################################################################
+#
 # "Set Terminal"
 #
 # setTerm FG[ALIAS/BYTE] BG[ALIAS/BYTE] BOLD[0-1]  
@@ -78,6 +108,8 @@ function setTerm() {
 #           
 # #################################################################################
 
+  # Essentially the same as secho, but it won't reset colours, hense the name.
+  # Also differs in it will change BG colour if wanted
   echo -ne $(tput setaf $(c "$1")); echo -ne $(tput setab $(c "$2")) 
   if [ $3 = 1 ]; then echo -ne $(tput bold); fi 
 
@@ -98,27 +130,29 @@ function setTextColour() {
 #           
 # #################################################################################
 
+  local bflag=0
   fg=$1
   bg=$2
   b=$3
 
+  if [[ ! $b = 1 || ! $b = 0 ]]; then b=0; fi  
+
   if [[ $fg =~ ^[[:digit:]] ]]; then   
     if [[ $fg -gt 255 || $fg -lt 0 ]]; then
       echo -ne "Warning - Value must be valid alias or numerical value between 0 and 255." 1>&2 
-      fg=$(c white)
+      echo -en $(tput setaf sgr0)
+      bflag=1
     fi
   fi  
   
   if [[ $bg =~ ^[[:digit:]] ]]; then   
     if [[ $bg -gt 255 || $bg -lt 0 ]]; then
      echo -ne "Warning - Value must be valid alias or numerical value between 0 and 255." 1>&2  
-     bg=$(c black)
+     echo -ne $(tput setab sgr0)
+     bflag=1
     fi
   fi   
 
-  if [[ ! $b = 1 || ! $b = 0 ]]; then b=0; fi  
-  setTerm $fg $bg $b 
+  if [ bflag = 0]; then setTerm $fg $bg $b; fi  
 
 }
-
-
